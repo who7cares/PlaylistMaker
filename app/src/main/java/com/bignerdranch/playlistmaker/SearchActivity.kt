d1f35b59e0334696703659322e29c24cb0329dc4
Practicuum <drensy@rambler.ru>
2024-12-08 13:02:29 +0300

    добавлен TextInputEditText в SearchActivity

--- app/src/main/java/com/bignerdranch/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/bignerdranch/playlistmaker/SearchActivity.kt
@@ -1,16 +1,90 @@
 package com.bignerdranch.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var textInputLayout:TextInputLayout
+    private lateinit var textInputEditText:TextInputEditText
+    private lateinit var buttonArrowBack:ImageView
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
+        textInputLayout = findViewById(R.id.search_textInputLayout)
+        textInputEditText = findViewById(R.id.search_textInputEditText)
+        buttonArrowBack = findViewById(R.id.arrow_back_search)
+
+
+        buttonArrowBack.setOnClickListener {
+            val intent = Intent(this@SearchActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // убираем фокус при нажатии enter
+        textInputEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                textInputEditText.clearFocus()
+
+                // Скрываем клавиатуру
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(textInputEditText.windowToken, 0)
+
+                true
+            } else {
+                false
+            }
+        }
+
+        // убираем фокус при нажатии за пределами бокса
+        textInputLayout.setOnTouchListener { v, event ->
+            // Проверяем, если касание произошло за пределами поля ввода
+            val rect = Rect()
+            textInputLayout.getGlobalVisibleRect(rect)
+            if (!rect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                textInputEditText.clearFocus()
+
+                // Скрываем клавиатуру
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(textInputEditText.windowToken, 0)
+            }
+            false
+        }
+
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is TextInputEditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
