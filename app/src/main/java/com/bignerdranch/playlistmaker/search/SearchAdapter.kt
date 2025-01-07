52209c24f50ac7f96eb29c228956a0580b4d5bf0
popka <drensymax@gmail.com>
2025-01-07 20:04:51 +0300

    - класс SearchViewHolder объединен с классом SearchAdapter

--- app/src/main/java/com/bignerdranch/playlistmaker/search/SearchAdapter.kt
+++ app/src/main/java/com/bignerdranch/playlistmaker/search/SearchAdapter.kt
@@ -2,14 +2,16 @@ package com.bignerdranch.playlistmaker.search
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.playlistmaker.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class SearchAdapter(private var tracks: List<Track>) : RecyclerView.Adapter<SearchViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
-        return SearchViewHolder(itemView)
-    }
+class SearchAdapter(private var tracks: List<Track>) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder = SearchViewHolder(parent)
 
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
         holder.bind(tracks[position])
@@ -22,4 +24,28 @@ class SearchAdapter(private var tracks: List<Track>) : RecyclerView.Adapter<Sear
         notifyDataSetChanged()
     }
 
+    class SearchViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+        LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.track_view, parent, false)
+    ) {
+
+        private val trackNameView: TextView = itemView.findViewById(R.id.trackName)
+        private val artistNameView: TextView = itemView.findViewById(R.id.artistName)
+        private val trackTimeView: TextView = itemView.findViewById(R.id.trackTime)
+        private val artworkUrl100View: ImageView = itemView.findViewById(R.id.artworkUrl100)
+
+        fun bind(model: Track) {
+            trackNameView.text = model.trackName
+            artistNameView.text = model.artistName
+            trackTimeView.text = model.trackTime
+
+            Glide.with(itemView)
+                .load(model.artworkUrl100)
+                .centerCrop()
+                .transform(RoundedCorners(2))
+                .placeholder(R.drawable.placeholder_search)
+                .into(artworkUrl100View)
+        }
+    }
 }
