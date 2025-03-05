bb121101280c0d1e8109e62982463e5ab6062201
popka <drensymax@gmail.com>
2025-03-05 11:50:57 +0300

    на пул

--- app/src/main/java/com/bignerdranch/playlistmaker/AudioPlayer.kt
+++ app/src/main/java/com/bignerdranch/playlistmaker/AudioPlayer.kt
@@ -130,17 +130,7 @@ class AudioPlayer : AppCompatActivity() {
         }
 
 
-//        playPauseButton.setOnClickListener {
-//            val currentDrawable = playPauseButton.drawable
-//            val pauseDrawable = ContextCompat.getDrawable(this, R.drawable.pause_icon)
-//
-//            // Сравниваем состояния drawable, а не сами объекты
-//            if (currentDrawable.constantState == pauseDrawable?.constantState) {
-//                playPauseButton.setImageResource(R.drawable.play_arrow_icon)
-//            } else {
-//                playPauseButton.setImageResource(R.drawable.pause_icon)
-//            }
-//        }
+
 
         addToLikeButton.setOnClickListener {
             val currentDrawable = addToLikeButton.drawable
